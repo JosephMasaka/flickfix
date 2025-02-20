5c871f18461371fe7354adb5674abb77b48398e5
JosephMasaka <masakajoseph14@gmail.com>
2025-02-20 14:22:35 +0300

    simple cosmetic changes: showcase

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit, inject } from '@angular/core';
 import { MovieService } from '../../services/movie/movie.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { ShowcaseComponent } from '../../components/home/showcase/showcase.component'
+import { ShowcaseComponent } from '../../components/home/showcase/showcase.component';
+import { LatestTvSeriesComponent } from '../../components/home/latest-tv-series/latest-tv-series.component';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss', 
-  imports: [CommonModule, HttpClientModule, ShowcaseComponent]
+  imports: [CommonModule, HttpClientModule, ShowcaseComponent, LatestTvSeriesComponent],
 })
 export class HomeComponent implements OnInit {
   movieService = inject(MovieService);
