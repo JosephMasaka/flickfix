d8d4fa8e69b3bff60e09990cbc0ac67efef59d18
JosephMasaka <masakajoseph14@gmail.com>
2025-02-25 08:31:04 +0300

    select related movie: movie component fix, added loader

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import axios from 'axios';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 
 const API_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'eeb61bdc3a426b35a6564e7cfce2e1bd';
@@ -15,37 +14,57 @@ const API_KEY = 'eeb61bdc3a426b35a6564e7cfce2e1bd';
   imports: [CommonModule]
 })
 export class MovieComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-
   movie: any = null;
   relatedMovies: any[] = [];
-  router = inject(Router);
-
-  async ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    await this.fetchMovieDetails(id);
-    await this.fetchRelatedMovies(id);
-  }
+  isLoading: boolean = true;
+  isLoadingRelated: boolean = true;
+  route = inject(ActivatedRoute);
+  router = inject(Router); // Inject Router to navigate programmatically
 
   async fetchMovieDetails(id: string) {
+    this.isLoading = true;
     try {
       const response = await axios.get(`${API_URL}/movie/${id}?api_key=${API_KEY}`);
       this.movie = response.data;
+      await this.fetchRelatedMovies(id);
+      this.isLoading = false;
     } catch (error) {
       console.error('Error fetching movie details:', error);
+      this.isLoading = false;
+    } finally {
+      this.isLoading = false;
     }
   }
 
   async fetchRelatedMovies(id: string) {
     try {
-      const response = await axios.get(`${API_URL}/movie/${id}/similar?api_key=${API_KEY}`);
-      this.relatedMovies = response.data.results.slice(0, 10); // Get top 10 related movies
+      const genreId = this.movie.genres[0].id;
+      // const response = await axios.get(`${API_URL}/movie/${id}/similar?api_key=${API_KEY}`);
+      const response = await axios.get(`${API_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`);
+      this.relatedMovies = response.data.results || [];
+      this.isLoadingRelated = false;
     } catch (error) {
       console.error('Error fetching related movies:', error);
+      this.isLoadingRelated = false;
+    } finally {
+      this.isLoadingRelated = false;
     }
   }
 
-  goToMovie(id: number) {
-    this.router.navigate(['/movie', id]); 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const movieId = params.get('id');
+      if (movieId) {
+        this.fetchMovieDetails(movieId);
+        this.fetchRelatedMovies(movieId);
+      }
+    });
+  }
+
+  goToMovie(movieId: number) {
+    this.router.navigate(['/movie', movieId]); // Navigate to selected movie
+
+    // Scroll to top smoothly
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
